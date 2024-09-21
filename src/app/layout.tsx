517fb4769f661066010b0b9853716d4622b4f315
saikumar305 <saikumarrachakonda305@gmail.com>
2024-09-21 22:37:11 +0530

    added big text component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { repositoryName } from "@/prismicio";
 import "./app.css";
 import Header from "@/components/Header";
 import ViewCanvas from "@/components/ViewCanvas";
+import Footer from "@/components/Footer";
 
 const myFont = localFont({
   src: "../../public/fonts/Alpino-Variable.woff2",
@@ -27,6 +28,7 @@ export default function RootLayout({
           {children}
           <ViewCanvas />
         </main>
+        <Footer />
       </body>
       <PrismicPreview repositoryName={repositoryName} />
     </html>
